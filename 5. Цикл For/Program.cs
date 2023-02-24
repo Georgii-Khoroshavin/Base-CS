2e5ff9a5fdecd9f911a47cd22b3cc74e110f6e27
Андрей Боев <a.boev.it@gmail.com>
2023-02-24 15:26:42 +0300

    Цикл For Оператор Switch

---
+++ 5. Цикл For/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Цикл_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // for ([Создание переменной счётчика];[Условие выполнения цикла];[Изменение переменной счётчика])
+            // Все разделы необязательны
+            // ++ - инкремент (увеличение на 1)
+            // -- - декремент (уменьшение на 1)
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Оператор выбора switch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Оператор_выбора_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывести название месяца по его номеру (1 - Январь, 2 - Февраль ...)
+            Console.Write("Введите номер месяца: ");
+            int month = int.Parse(Console.ReadLine());
+            // В switch передаётся какая-то переменная,
+            // которую проверяем на соответствие определённым значениям
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                // Если ни одно значени не совпало
+                default:
+                    Console.WriteLine("Введены неверные данные!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
